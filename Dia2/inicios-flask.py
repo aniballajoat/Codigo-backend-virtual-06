f3d6bdda4ba936e335e3d6114709aafeb18955f7
Anibal <aniballajo.at@gmail.com>
2021-05-27 03:50:44 -0500

    inicios con flask y postman

---
+++ Dia2/inicios-flask.py
@@ -0,0 +1,71 @@
+from flask import Flask, request
+
+#print(__name__)
+app = Flask(__name__)
+productos = []
+
+@app.route("/")
+def inicio():
+    print("Me hicieron un llamado")
+    return "Salidos desde mi API"
+
+@app.route("/productos", methods = ['GET','POST'])
+def gestion_productos():
+    print(request.method)
+    if request.method == "POST":
+        data = request.get_json()
+        print(data)
+        productos.append(data)
+        return{
+            "message": "Producto creado exitosamente",
+            "content": data
+        }, 201
+    elif request.method == "GET":
+        return{
+            "message": "Estos son los productos registrados",
+            "content": productos
+        },200
+
+@app.route("/productos/<int:id>",methods=['PUT','DELETE','GET'])
+def gestion_producto(id):
+    print(id)
+    if len(productos)<=id:
+        return{
+            "message": "Producto no encontrado"
+        }, 404
+    if request.method == 'GET':
+        """    
+            try:
+                return{
+                    "content": productos[id]
+                },200
+            except:
+                return{
+                    "message": "Producto no encontrado"
+                },404
+        """
+        return{
+            "content": productos[id]
+        },200
+    
+
+    elif request.method == "DELETE":
+        productos.pop(id)
+        return{
+            "message": "Producto eliminado exitosamente"
+        }
+    elif request.method == "PUT":
+        data = request.get_json()
+        productos[id] = data
+        return{
+            "message": "Producto actualizado exitosamente",
+            "content": productos[id]
+        }
+    return "ok"
+
+@app.route("/productos/buscar")
+def buscar_productos():
+    print(request.args.get("nombre", "NO HAY"))
+    return "ok"
+
+app.run(debug=True)
---
+++ README.md
@@ -0,0 +1,12 @@
+Para crear un entorno virtual:
+
+python -m venv <nombre_entorno>
+
+esto creara una carpeeta con una serie de subcarpetas para que funcione el entorno
+
+- Windows
+.\nombre_entorno\Scripts\activate
+
+Ahora instalamos Flask
+
+pip install flask
