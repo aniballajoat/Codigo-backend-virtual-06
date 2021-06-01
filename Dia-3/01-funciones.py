c7f1828278c5ec655a728508505fde02150c0b67
Anibal <aniballajo.at@gmail.com>
2021-06-01 16:39:27 -0500

    se limpio la rama 3

--- Dia-3/01-funciones.py
+++
@@ -1,8 +0,0 @@
-def sumatoria(num1, num2):
-    ans = num1+num2
-    if ans%2==0:
-        print("es par, y su suma y mitad es ",int(ans/2))
-    else:
-        print("no es par, y su suma es ",ans)
-
-sumatoria(2,2)
