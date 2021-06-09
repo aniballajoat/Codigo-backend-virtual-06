e1f6e0ac6440a6fc86630b491d6367dd1c06f6b1
Anibal <aniballajo.at@gmail.com>
2021-06-08 20:28:38 -0500

    creacion usuario controller

--- monedero/models/usuario.py
+++ monedero/models/usuario.py
@@ -1,5 +1,6 @@
 from config.conexion_bd import base_de_datos
 from sqlalchemy import Column, types, orm
+import bcrypt
 
 class UsuarioModel(base_de_datos.Model):
     __tablename__="usuarios"
@@ -8,7 +9,34 @@ class UsuarioModel(base_de_datos.Model):
                            unique=True, autoincrement=True, nullable=False)
     usuarioNombre = Column(name='nombre', type_=types.String(45), nullable=False)
     usuarioApellido = Column(name='apellido', type_=types.String(45), nullable=False)
-    usuarioCorreo = Column(name='correo', type_=types.String(25), nullable=False)
+    usuarioCorreo = Column(name='correo', type_=types.String(25), nullable=False, unique=True)
     usuarioPassword = Column(name='password', type_=types.TEXT, nullable=False)
 
     movimientos = orm.relationship('MovimientoModel', backref='movimientoUsuario')
+
+    def __init__(self, nombre, apellido, correo, password):
+        self.usuarioNombre = nombre
+        self.usuarioApellido = apellido
+        self.usuarioCorreo = correo
+        # ENCRIPTAR PASSWORD
+        # primero agarro el password y lo convierto en bytes mediante el formato de escritura utf-8
+        passwordBytes = bytes(password, "utf-8")
+        # el metodo hashpw agarra la contraseña y un salt generado aleatoriamente para fusionarlos y luego devolver
+        # la contraseña hasheada(en forma de hash)
+        passwordHash = bcrypt.hashpw(passwordBytes, bcrypt.gensalt())
+        # el metodo decode convierte el tipo de dato bytes a str
+        passwordString = passwordHash.decode("utf-8")
+        self.usuarioPassword = passwordString
+        # self.usuarioPassword = password
+
+    def save(self):
+        base_de_datos.session.add(self)
+        base_de_datos.session.commit()
+
+    def json(self):
+        return{
+            "usuarioId": self.usuarioId,
+            "usuarioNombre": self.usuarioNombre,
+            "usuarioApellido": self.usuarioApellido,
+            "usuarioCorreo": self.usuarioCorreo
+        }
