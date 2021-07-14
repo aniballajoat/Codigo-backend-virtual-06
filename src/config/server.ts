a85aaceea0d3f26d0e889fc1fa0c767dd216ca67
Anibal <aniballajo.at@gmail.com>
2021-07-13 20:20:36 -0500

    agregue documentacion swagger

--- src/config/server.ts
+++ src/config/server.ts
@@ -1,4 +1,4 @@
-import express, { Request, Response, Express } from "express";
+import express, { Request, Response, Express, NextFunction } from "express";
 import morgan from "morgan";
 import { json } from "body-parser";
 import { tipoRouter } from "../routes/tipo";
@@ -8,7 +8,10 @@ import conexion from "./sequelize";
 import { productoRouter } from "../routes/producto";
 import { imagenRouter } from "../routes/imagen";
 import { movimientoRouter } from "../routes/movimiento";
+import documentation from "./swagger.json"
+import swaggerUI from "swagger-ui-express"
 
+require("dotenv").config();
 export default class Server {
   app: Express;
   port: string = "";
@@ -17,6 +20,7 @@ export default class Server {
     this.app = express();
     this.port = process.env.PORT || "8000";
     this.bodyParser();
+    this.CORS();
     this.rutas();
   }
 
@@ -25,10 +29,24 @@ export default class Server {
     this.app.use(morgan("dev"));
   }
 
+  CORS(){
+    this.app.use((req: Request, res: Response, next: NextFunction)=>{
+      // Access-Control-Allow-Origin => indicar que origenes (dominios pueden acceder a mi API)
+      res.header('Access-Control-Allow-Origin','*')
+      // Access-Control-Allow-Headers => indicar que tipos de cabeceras pueden ser enviadas
+      res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization')
+      // Access-Control-Allow-Methods => indica que metodos pueden ser intentar acceder a mi backend
+      res.header('Access-control-Allow-Methods', 'GET, POST, PUT, DELETE')
+      // si es que cumple el origen (dominio), el header y el metodo entonces daremos paso al controlador solicitado
+      next();
+    });
+  }
+
   rutas() {
     this.app.get("/", (req: Request, res: Response) => {
       res.send("Bienvenido a la api de zapateria");
     });
+    this.app.use("/docs", swaggerUI.serve, swaggerUI.setup(documentation));
     this.app.use(tipoRouter);
     this.app.use(accionRouter);
     this.app.use(usuarioRouter);
