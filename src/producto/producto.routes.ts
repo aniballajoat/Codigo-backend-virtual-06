7d0d3eaacc038b2b701ebad89363bc0726ade36d
Anibal <aniballajo.at@gmail.com>
2021-07-16 20:51:11 -0500

    :sparkles: crud productos
    agregue paginador, buscador y eliminador

--- src/producto/producto.routes.ts
+++ src/producto/producto.routes.ts
@@ -1,8 +1,21 @@
 import { Router } from "express";
-import { crearProducto, mostrarProductos } from "./producto.controller";
+import { actualizarProductoDto } from "./dto.request.ts";
+import { 
+    actualizarProducto, 
+    crearProducto, 
+    eliminarProducto, 
+    mostrarProductos,
+ } from "./producto.controller";
 
 export const productoRouter = Router();
 
-productoRouter.route("/productos")
-                    .post(crearProducto)
-                    .get(mostrarProductos);
+productoRouter
+        .route("/productos")
+        .post(crearProducto)
+        .get(mostrarProductos);
+
+productoRouter
+        .route("/productos/:id")
+        .put(actualizarProducto)
+        .put(actualizarProductoDto,actualizarProducto)
+        .delete(eliminarProducto);
