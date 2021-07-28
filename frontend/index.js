372c65f34374718fc2a54df725041d7a1b04939a
Anibal <aniballajo.at@gmail.com>
2021-07-27 21:23:09 -0500

    creacion rama backend

--- frontend/index.js
+++
@@ -1,7 +0,0 @@
-const socket = io("http://127.0.0.1:8000");
-const btnEnviar = document.getElementById("btnIngresar");
-const username = document.getElementById("username");
-
-btnIngresar.addEventListener("click", (evento)=>{
-    socket.emit('registrar', {username: username})
-});
